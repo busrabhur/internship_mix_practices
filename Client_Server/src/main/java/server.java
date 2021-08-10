cfd1e8c28fc10387091c4063ea406aef3ede1ff9
Büşra Buhur <61162439+busrabhur@users.noreply.github.com>
2021-08-10 18:44:28 +0300

    new

---
+++ Client_Server/src/main/java/server.java
@@ -0,0 +1,14 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+ class server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss=new ServerSocket(4999);
+        Socket s=ss.accept();
+        System.out.println("client connected");
+   
+}
+}
